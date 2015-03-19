a5690f430001047c02605be94d8c27b077268ad1
ybdesire <ybdesire@gmail.com>
2015-03-19 08:38:26 +0800

    Button click event by adding click listener function.

--- HelloWorld/app/src/main/java/com/example/biny/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/biny/helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -14,6 +15,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn=(Button)findViewById(R.id.button2);//find button by id(defined at activity_main.xml)
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView txt=(TextView)findViewById(R.id.textView);//find output label by id
+                txt.setText("Hello world! click by button 2");
+            }
+        });
+
     }
 
 
