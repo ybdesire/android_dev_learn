ac75437d19549adb7b1f3d39cf9000d8253a8244
ybdesire <ybdesire@gmail.com>
2016-04-20 14:59:39 +0800

    Add button click event process.

--- HelloWorld/app/src/main/java/com/example/ybdesire/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/ybdesire/helloworld/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ybdesire.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btn1ClickEvent(View target){
+        TextView txt=(TextView)findViewById(R.id.textView);//find output label by id
+        txt.setText("Hello world! click by button!!");
+
     }
 }
