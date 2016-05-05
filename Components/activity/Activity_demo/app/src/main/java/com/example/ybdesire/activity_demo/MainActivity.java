c40c92ba7713381f8dee6dd51ca9813096d66a3f
ybdesire <ybdesire@gmail.com>
2016-05-05 14:45:03 +0800

    Active another activity.

--- Components/activity/Activity_demo/app/src/main/java/com/example/ybdesire/activity_demo/MainActivity.java
+++ Components/activity/Activity_demo/app/src/main/java/com/example/ybdesire/activity_demo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ybdesire.activity_demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view) {
+        // Do something in response to button click
+        TextView txt=(TextView)findViewById(R.id.textView);//find output label by id
+        txt.setText("Hello world! click by button ");
+        Intent intnt = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(intnt);
+
+    }
+
 }
