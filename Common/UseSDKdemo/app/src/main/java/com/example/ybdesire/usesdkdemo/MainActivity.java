dcc52a1fd34df508d42674d66380baf51f89ffda
ybdesire <ybdesire@gmail.com>
2016-08-15 16:59:28 +0800

    Demo for calling functions at another class.

--- Common/UseSDKdemo/app/src/main/java/com/example/ybdesire/usesdkdemo/MainActivity.java
+++ Common/UseSDKdemo/app/src/main/java/com/example/ybdesire/usesdkdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ybdesire.usesdkdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // get string from class SdkHello member function
+        SdkHello sh = new SdkHello();
+        String str = sh.getStr();
+        // display the string at TextView
+        TextView t = (TextView)findViewById(R.id.texto);
+        t.setText(str);
     }
 }
---
+++ Common/UseSDKdemo/app/src/main/java/com/example/ybdesire/usesdkdemo/SdkHello.java
@@ -0,0 +1,11 @@
+package com.example.ybdesire.usesdkdemo;
+
+/**
+ * Created by ybdesire on 2016/8/15.
+ */
+public class SdkHello {
+    public String getStr()
+    {
+        return "Hello Hello World";
+    }
+}
