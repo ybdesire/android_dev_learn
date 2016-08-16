a5b9cb5619d12d8084a532b8ed5a8727fc37846f
ybdesire <ybdesire@gmail.com>
2016-08-16 18:06:29 +0800

    Detection root or not, and TextView display info at center.

--- Common/SuperMonitor/app/src/main/java/com/personal/ybdesire/supermonitor/MainActivity.java
+++ Common/SuperMonitor/app/src/main/java/com/personal/ybdesire/supermonitor/MainActivity.java
@@ -2,6 +2,7 @@ package com.personal.ybdesire.supermonitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RootUtil rootutil = new RootUtil();
+        String str = "Rooted: No";
+        if(rootutil.isDeviceRooted())
+        {
+            str = "Rooted: Yes";
+        }
+        // display the string at TextView
+        TextView t = (TextView)findViewById(R.id.rootinfo);
+        t.setText(str);
+
     }
 }
---
+++ Common/SuperMonitor/app/src/main/java/com/personal/ybdesire/supermonitor/RootUtil.java
@@ -0,0 +1,44 @@
+package com.personal.ybdesire.supermonitor;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ybdesire on 2016/8/16.
+ * Code from: http://stackoverflow.com/questions/1101380/determine-if-running-on-a-rooted-device
+ */
+public class RootUtil {
+    public static boolean isDeviceRooted() {
+        return checkRootMethod1() || checkRootMethod2() || checkRootMethod3();
+    }
+
+    private static boolean checkRootMethod1() {
+        String buildTags = android.os.Build.TAGS;
+        return buildTags != null && buildTags.contains("test-keys");
+    }
+
+    private static boolean checkRootMethod2() {
+        String[] paths = { "/system/app/Superuser.apk", "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
+                "/system/bin/failsafe/su", "/data/local/su" };
+        for (String path : paths) {
+            if (new File(path).exists()) return true;
+        }
+        return false;
+    }
+
+    private static boolean checkRootMethod3() {
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec(new String[] { "/system/xbin/which", "su" });
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            if (in.readLine() != null) return true;
+            return false;
+        } catch (Throwable t) {
+            return false;
+        } finally {
+            if (process != null) process.destroy();
+        }
+    }
+
+}
