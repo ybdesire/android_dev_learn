a57bb0235856881039fdeeab9b1cc381e1f18581
ybdesire <ybdesire@gmail.com>
2016-09-15 08:56:57 +0800

    Simple ListView demo.

--- Common/SuperMonitor/app/src/main/java/com/personal/ybdesire/supermonitor/SysSecActivity.java
+++ Common/SuperMonitor/app/src/main/java/com/personal/ybdesire/supermonitor/SysSecActivity.java
@@ -2,6 +2,8 @@ package com.personal.ybdesire.supermonitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SysSecActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SysSecActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sys_sec);
+
+        String [] strs = new String[]{"123","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345"};
+        ListView lv = (ListView) findViewById(R.id.listview_sys_sec);
+        lv.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, strs));
     }
 }
