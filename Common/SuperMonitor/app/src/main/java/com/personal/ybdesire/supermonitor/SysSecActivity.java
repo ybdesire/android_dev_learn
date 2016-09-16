4121a15423478d34d843787dbb508861a5c75da6
ybdesire <ybdesire@gmail.com>
2016-09-17 07:56:34 +0800

    Add Title and Text element to ListView.

--- Common/SuperMonitor/app/src/main/java/com/personal/ybdesire/supermonitor/SysSecActivity.java
+++ Common/SuperMonitor/app/src/main/java/com/personal/ybdesire/supermonitor/SysSecActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SysSecActivity extends AppCompatActivity {
 
@@ -12,11 +16,31 @@ public class SysSecActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sys_sec);
 
-        String [] strs = new String[]{"123","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345","234","345"};
 
         ListView lv = (ListView) findViewById(R.id.listview_sys_sec);
-        lv.setAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_checked, strs));
+
+        String [] vuls_str = new String[]{
+                //"KillingInTheNameOf", "Exploid", "RageAgainstTheCage", "Zimperlich"
+                "KillingInTheNameOf", "Exploid", "RageAgainstTheCage", "Zimperlich", "Exploid2", "RageAgainstTheCage2", "Zimperlich2", "Exploid3", "RageAgainstTheCage3", "Zimperlich3", "Exploid4", "RageAgainstTheCage4", "Zimperlich4"
+        };
+
+
+        ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String,     Object>>();
+
+        for(int i=0;i<vuls_str.length;i++)
+        {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("ItemTitle", vuls_str[i]);
+            map.put("ItemText", "Android vulnerability");
+            listItem.add(map);
+        }
+
+        SimpleAdapter adpt = new SimpleAdapter(this,listItem,
+                R.layout.sys_sec_item,
+                new String[] {"ItemTitle", "ItemText"},
+                new int[] {R.id.ItemTitle,R.id.ItemText}
+        );
+        lv.setAdapter( adpt);
         lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
     }
