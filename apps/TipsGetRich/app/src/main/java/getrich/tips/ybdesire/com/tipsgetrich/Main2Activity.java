f666a4718caf90ddf79a1b9da3801914dc89706d
ybdesire <ybdesire@gmail.com>
2018-10-15 19:20:21 +0800

    add admob banner and interstitial ad

--- apps/TipsGetRich/app/src/main/java/getrich/tips/ybdesire/com/tipsgetrich/Main2Activity.java
+++ apps/TipsGetRich/app/src/main/java/getrich/tips/ybdesire/com/tipsgetrich/Main2Activity.java
@@ -1,20 +1,36 @@
 package getrich.tips.ybdesire.com.tipsgetrich;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
 
 public class Main2Activity extends AppCompatActivity {
+
+    private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // init and load banner ad
+        MobileAds.initialize(this, "ca-app-pub-8100413825150401~9396888732");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-8100413825150401/1718543413");
 
         // get page num from main activity
         Bundle b = getIntent().getExtras();
@@ -31,17 +47,17 @@ public class Main2Activity extends AppCompatActivity {
             setTitle(str);
         }
 
-
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button button= (Button) findViewById(R.id.btn_back);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "replace your action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+                finish();
             }
         });
+
+
     }
 
 }
--- apps/TipsGetRich/app/src/main/java/getrich/tips/ybdesire/com/tipsgetrich/MainActivity.java
+++ apps/TipsGetRich/app/src/main/java/getrich/tips/ybdesire/com/tipsgetrich/MainActivity.java
@@ -7,14 +7,28 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // init and load banner ad
+        MobileAds.initialize(this, "ca-app-pub-8100413825150401~9396888732");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-8100413825150401/1718543413");
 
         //content to display
         String[] tab_names = getResources().getStringArray(R.array.tabs_names);
@@ -37,13 +51,14 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
                 //点击后在标题上显示点击了第几行
                 setTitle("你点击了第"+arg2+"行");
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
                 // start another acitvity with parameters
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
                 Bundle b = new Bundle();
                 b.putInt("page", arg2); //Your key-value
                 intent.putExtras(b); //Put your key-value
                 startActivity(intent);
-                finish();
             }
         });
 
